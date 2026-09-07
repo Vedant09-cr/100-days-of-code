0349c2acb354df9a0e4e95a863ccdf42852109b5
Vedant09-cr <vedantsomani09@gmail.com>
2026-09-07 19:24:13 +0530

    q8

---
+++ day4/q8.c
@@ -0,0 +1,29 @@
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+Sum=15
+
+Input 2:
+10
+Output 2:
+Sum=55
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+
+    return 0;
+}
